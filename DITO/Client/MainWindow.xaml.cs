1e2c372a6081673f25db284456e6ef84d733a897
Alex <34625198+al3xfischer@users.noreply.github.com>
2019-10-31 10:51:06 +0100

    mvvm command

--- DITO/Client/MainWindow.xaml.cs
+++ DITO/Client/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Net.Http;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Grpc.Net.Client;
 using Server;
 
@@ -27,7 +14,7 @@ namespace Client
 
         public MainWindow()
         {
-            var channel = GrpcChannel.ForAddress("https://localhost:5001/");
+            var channel = GrpcChannel.ForAddress("http://localhost:5000/");
             var client = new Greeter.GreeterClient(channel);
 
             var reply = client.SayHello(new HelloRequest { Name = "Test" });
