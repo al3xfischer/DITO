b3b1ad95b3e7f930c27f3d5fbe71961471dc88ab
Alex <34625198+al3xfischer@users.noreply.github.com>
2019-11-11 16:00:37 +0100

    client to client test

--- DITO/Client/Services/Provider/ClientToClientService.cs
+++ DITO/Client/Services/Provider/ClientToClientService.cs
@@ -5,14 +5,17 @@ namespace Client.Services.Provider
 {
     public class ClientToClientService
     {
+        private readonly Greeter.GreeterClient client;
         public ClientToClientService()
         {
             var port = 5001;
-            var channel = new Channel("localhost:5001", ChannelCredentials.Insecure);
+            var channel = new Channel("10.0.0.4:5001", ChannelCredentials.Insecure);
+            client = new Greeter.GreeterClient(channel);
+        }
 
-            var client = new Greeter.GreeterClient(channel);
-            var reply = client.SayHello(new HelloRequest {  Name = "alex"});
-
+        public string CallServer()
+        {
+             return client.SayHello(new HelloRequest {  Name = "alex"}).Message;
         }
     }
 }
