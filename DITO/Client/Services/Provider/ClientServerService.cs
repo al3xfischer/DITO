14835aa31d51ccef376773c5bd736af78b6d38a4
Alex <34625198+LeqxCodes@users.noreply.github.com>
2019-11-11 15:35:36 +0100

    client-side server

---
+++ DITO/Client/Services/Provider/ClientServerService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Grpc.Core;
+using System.Threading.Tasks;
+using Server;
+
+namespace Client.Services.Provider
+{
+    class GreeterIml : Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply { Message = "Hello" + request.Name });
+        }
+    }
+
+    public class ClientServerService
+    {
+        public ClientServerService()
+        {
+            var port = 5001;
+            var server = new Grpc.Core.Server
+            {
+                Services = { Greeter.BindService(new GreeterIml()) },
+                Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+            };
+
+            server.Start();
+        }
+    }
+}
---
+++ DITO/Client/Services/Provider/ClientToClientService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+using Server;
+
+namespace Client.Services.Provider
+{
+    public class ClientToClientService
+    {
+        public ClientToClientService()
+        {
+            var port = 5001;
+            var channel = new Channel("localhost:5001", ChannelCredentials.Insecure);
+
+            var client = new Greeter.GreeterClient(channel);
+            var reply = client.SayHello(new HelloRequest {  Name = "alex"});
+
+        }
+    }
+}
