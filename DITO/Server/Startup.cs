13d73ca4797ddf9b827110a88c234afc382a5e67
Alex <34625198+al3xfischer@users.noreply.github.com>
2019-11-17 15:19:18 +0100

    rm greeter

--- DITO/Server/Startup.cs
+++ DITO/Server/Startup.cs
@@ -37,7 +37,6 @@ namespace Server
             {
                 // Communication with gRPC endpoints must be made through a gRPC client.
                 // To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909
-                endpoints.MapGrpcService<GreeterService>();
                 endpoints.MapGrpcService<SignUpServiceImpl>();
                 endpoints.MapGrpcService<DeleteFilesServiceImpl>();
             });
